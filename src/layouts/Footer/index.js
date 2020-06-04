c4567725af504f398cf5f2fb70fb439b3371e5a1
David C <davidcoelho058@gmail.com>
2020-06-04 03:53:58 +0100

    Create Footer & Header

--- src/layouts/Footer/index.js
+++ src/layouts/Footer/index.js
@@ -1,5 +1,54 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+
+import twitterIcon from '../../assets/icons/twitter.svg';
+import instagramIcon from '../../assets/icons/instagram.svg';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    '& header': {
+      boxShadow: 'none',
+    },
+  },
+  instagram: {
+    marginRight: theme.spacing(3),
+  },
+}));
 
 export const Footer = () => {
-  return <div>Footer</div>;
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color="transparent">
+        <Container>
+          <Toolbar>
+            <Grid container alignItems="center">
+              <Grid item sm={1} alignItems="center">
+                <img className={classes.instagram} src={instagramIcon} alt="" />
+                <img src={twitterIcon} alt="" />
+              </Grid>
+              <Grid item sm={4}>
+                <NavLink to="/about-us">About Us</NavLink>
+                <NavLink to="/how-works">How abe works</NavLink>
+                <NavLink to="/privacy">Privacy Policy</NavLink>
+                <NavLink to="/terms">Terms of Service</NavLink>
+              </Grid>
+              <Grid item sm={7}>
+                © 2020 PopOdds inc. Reserved. If you or someone you know has a
+                gambling problem and wants help, call 1-800-Gambler. You must be
+                21 years or older to place a bet.
+              </Grid>
+            </Grid>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </div>
+  );
 };
--- src/layouts/Header/index.js
+++ src/layouts/Header/index.js
@@ -1,5 +1,85 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+
+import logoWithText from '../../assets/icons/logo_with_text.svg';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    '& header': {
+      boxShadow: 'none',
+    },
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  navLinkGroup: {
+    '& a': {
+      fontFamily: 'Regular',
+      fontWeight: 600,
+      letterSpacing: '0.02em',
+      marginRight: theme.spacing(2),
+      marginLeft: theme.spacing(2),
+    },
+  },
+  loginBtn: {
+    marginLeft: 'auto',
+    marginRight: theme.spacing(1),
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2),
+  },
+}));
 
 export const Header = () => {
-  return <div>Header</div>;
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color="transparent">
+        <Container>
+          <Toolbar>
+            <img src={logoWithText} alt="" />
+            <Box className={classes.navLinkGroup}>
+              <NavLink activeClassName="active" to="/odds">
+                Odds
+              </NavLink>
+              <NavLink activeClassName="active" to="/futures">
+                Futures
+              </NavLink>
+              <NavLink activeClassName="active" to="/about">
+                How To Bet
+              </NavLink>
+              <NavLink activeClassName="active" to="/legalization">
+                Legalization
+              </NavLink>
+              <NavLink activeClassName="active" to="/insights">
+                Insights
+              </NavLink>
+              <NavLink activeClassName="active" to="/reviews">
+                Reviews
+              </NavLink>
+              <NavLink activeClassName="active" to="/deals">
+                Deals
+              </NavLink>
+            </Box>
+            <Button className={classes.loginBtn} color="inherit">
+              Log In
+            </Button>
+            <Button variant="contained" color="primary">
+              Sign Up
+            </Button>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </div>
+  );
 };
