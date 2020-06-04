dc2c96be8782a7682d9a67dc25e55b3f51e1cd47
David C <davidcoelho058@gmail.com>
2020-06-04 20:16:31 +0100

    Update global mui theme

--- src/theme.js
+++ src/theme.js
@@ -14,12 +14,56 @@ export default createMuiTheme({
   text: {
     primary: '#000000',
   },
+  typography: {
+    fontFamily: 'Barlow',
+    h1: {
+      fontWeight: 'bold',
+      fontSize: 48,
+      fontFamily: 'Regular',
+    },
+    h2: {
+      fontWeight: 'bold',
+      fontSize: 28,
+      fontFamily: 'Regular',
+    },
+    h3: {
+      fontWeight: 'bold',
+      fontSize: 20,
+      fontFamily: 'Regular',
+    },
+    subtitle1: {
+      fontSize: 18,
+      fontFamily: 'Barlow',
+      fontWeight: 300,
+    },
+    body1: {
+      fontSize: 16,
+      fontFamily: 'Barlow',
+      fontWeight: 300,
+    },
+  },
   overrides: {
     MuiButton: {
       root: {
         textTransform: 'none',
         borderRadius: 4,
         boxShadow: 'none',
+        minWidth: 'auto',
+      },
+    },
+    MuiListItemIcon: {
+      root: {
+        minWidth: 40,
+      },
+    },
+    MuiListItemText: {
+      secondary: {
+        color: 'black',
+        opacity: 0.9,
+        fontWeight: 300,
+        fontFamily: 'Barlow',
+        fontSize: 14,
+        lineHeight: '16px',
       },
     },
   },
